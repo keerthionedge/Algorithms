9707e6b770c41035af6857f207afe654da1e23e3
keerthi <ihtreekr@gmail.com>
2013-05-17 15:17:23 +0530

    problem 44: Pentagonal Numbers - Brute Force

---
+++ src/com/kee/example/PentagonalRunner.java
@@ -0,0 +1,12 @@
+package com.kee.example;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: keerthi
+ * Date: 5/16/13
+ * Time: 3:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PentagonalRunner {
+
+}
